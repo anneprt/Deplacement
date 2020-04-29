84e61963e2e1e96ff5fe05747141b62f0542fe5f
Anne <patesdegeek@gmail.com>
2020-04-29 22:34:23 +0200

    class main

---
+++ src/UnHerosSeDeplace.java
@@ -0,0 +1,18 @@
+public class UnHerosSeDeplace {
+    public static void main(String[] args) {
+        Voiture V = new Voiture(5, 5);
+        V.afficher();
+        V.seDeplacer();
+        V.afficher();
+
+        Ascenseur A = new Ascenseur(20, 5);
+        A.afficher();
+        A.seDeplacer();
+        A.afficher();
+
+        Fusee F = new Fusee(50, 0);
+        F.afficher();
+        F.seDeplacer();
+        F.afficher();
+    }
+}
